62ba623b286783e846736869e250789b15e24cd0
dipnoe <96998276+dipnoe@users.noreply.github.com>
2023-03-10 00:38:41 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -3,3 +3,10 @@ def up_letters(string: str) -> str:
     function returns the string in upper case
     """
     return string.upper()
+
+
+def title_words(string: str) -> str:
+    """
+    return words with title first letter
+    """
+    return string.title()
