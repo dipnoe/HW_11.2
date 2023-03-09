bc44b14189aa1fa76953e492e11e9e2a517e38f6
Elena Vagner <dipnoe@gmail.com>
2023-03-10 00:04:09 +0300

    'add second function'

--- str_func.py
+++ str_func.py
@@ -3,3 +3,10 @@ def up_letters(string: str) -> str:
     return the string in upper case
     """
     return string.upper()
+
+
+def title_word(string: str) -> str:
+    """
+    return the string with title first letter in each word
+    """
+    return string.title()
